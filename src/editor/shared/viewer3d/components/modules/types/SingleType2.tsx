c314e52dbe83de7e1888d04ef05e084cb5b963d7
jinsoo <sbbc212@gmail.com>
2025-10-12 20:10:32 +0900

    feat: 싱글 2단 옷장에 환기캡 추가
    - VentilationCap import 추가
    - DualType2와 동일한 위치에 환기캡 렌더링
    - 위치: 우측 132mm, 상단 115mm, 백패널 앞 17mm
    - 직경: 98mm
    - 드래그 중에는 렌더링하지 않음

--- src/editor/shared/viewer3d/components/modules/types/SingleType2.tsx
+++ src/editor/shared/viewer3d/components/modules/types/SingleType2.tsx
@@ -10,6 +10,7 @@ import { useUIStore } from '@/store/uiStore';
 import DoorModule from '../DoorModule';
 import { AdjustableFootsRenderer } from '../components/AdjustableFootsRenderer';
 import { ClothingRod } from '../components/ClothingRod';
+import { VentilationCap } from '../components/VentilationCap';
 
 /**
  * SingleType2 컴포넌트
@@ -410,6 +411,19 @@ const SingleType2: React.FC<FurnitureTypeProps> = ({
             });
           })()}
         </>
+      )}
+
+      {/* 환기캡 렌더링 */}
+      {!isDragging && (
+        <VentilationCap
+          position={[
+            innerWidth/2 - mmToThreeUnits(132),  // 우측 패널 안쪽으로 132mm
+            height/2 - basicThickness - mmToThreeUnits(115),  // 상단 패널 아래로 115mm
+            -depth/2 + backPanelThickness + mmToThreeUnits(17) + 0.01  // 백패널 앞쪽에 살짝 앞으로
+          ]}
+          diameter={98}
+          renderMode={renderMode}
+        />
       )}
         </group>
       )}
