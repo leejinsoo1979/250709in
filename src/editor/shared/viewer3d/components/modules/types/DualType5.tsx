1fd65610b7f46afad97ceb0c649702e22b3e475d
jinsoo <sbbc212@gmail.com>
2025-10-13 23:54:26 +0900

    fix: 스타일러장 우측 3D 내경 치수를 뒷면으로 완전히 이동 (도어 뒤로)

--- src/editor/shared/viewer3d/components/modules/types/DualType5.tsx
+++ src/editor/shared/viewer3d/components/modules/types/DualType5.tsx
@@ -856,8 +856,8 @@ const DualType5: React.FC<FurnitureTypeProps> = ({
                     {/* 가로 내경 수평선 */}
                     <Line
                       points={[
-                        [-rightWidth/2, sectionCenterY + sectionHeight/2 - basicThickness - 1.0, viewMode === '3D' ? rightAdjustedDepthForShelves/2 - 1.5 : rightDepth/2 + 1.0],
-                        [rightWidth/2, sectionCenterY + sectionHeight/2 - basicThickness - 1.0, viewMode === '3D' ? rightAdjustedDepthForShelves/2 - 1.5 : rightDepth/2 + 1.0]
+                        [-rightWidth/2, sectionCenterY + sectionHeight/2 - basicThickness - 1.0, viewMode === '3D' ? -rightAdjustedDepthForShelves/2 + 0.5 : rightDepth/2 + 1.0],
+                        [rightWidth/2, sectionCenterY + sectionHeight/2 - basicThickness - 1.0, viewMode === '3D' ? -rightAdjustedDepthForShelves/2 + 0.5 : rightDepth/2 + 1.0]
                       ]}
                       color={dimensionColor}
                       lineWidth={1}
@@ -869,7 +869,7 @@ const DualType5: React.FC<FurnitureTypeProps> = ({
                         position={[
                           0 + 0.01,
                           sectionCenterY + sectionHeight/2 - basicThickness - 1.2 - 0.01,
-                          rightAdjustedDepthForShelves/2 - 1.5 - 0.01
+                          -rightAdjustedDepthForShelves/2 + 0.5 - 0.01
                         ]}
                         fontSize={baseFontSize}
                         color="rgba(0, 0, 0, 0.3)"
@@ -884,7 +884,7 @@ const DualType5: React.FC<FurnitureTypeProps> = ({
                       position={[
                         0,
                         sectionCenterY + sectionHeight/2 - basicThickness - 1.2,
-                        viewMode === '3D' ? rightAdjustedDepthForShelves/2 - 1.5 : rightDepth/2 + 1.0
+                        viewMode === '3D' ? -rightAdjustedDepthForShelves/2 + 0.5 : rightDepth/2 + 1.0
                       ]}
                       fontSize={baseFontSize}
                       color={dimensionColor}
@@ -898,11 +898,11 @@ const DualType5: React.FC<FurnitureTypeProps> = ({
                     {/* 수평선 양끝 점 - 측면뷰에서 숨김 */}
                     {!(viewMode === '2D' && (view2DDirection === 'left' || view2DDirection === 'right')) && (
                       <>
-                        <mesh position={[-rightWidth/2, sectionCenterY + sectionHeight/2 - basicThickness - 1.0, viewMode === '3D' ? rightAdjustedDepthForShelves/2 - 1.5 : rightDepth/2 + 1.0]}>
+                        <mesh position={[-rightWidth/2, sectionCenterY + sectionHeight/2 - basicThickness - 1.0, viewMode === '3D' ? -rightAdjustedDepthForShelves/2 + 0.5 : rightDepth/2 + 1.0]}>
                           <sphereGeometry args={[0.05, 8, 8]} />
                           <meshBasicMaterial color={dimensionColor} />
                         </mesh>
-                        <mesh position={[rightWidth/2, sectionCenterY + sectionHeight/2 - basicThickness - 1.0, viewMode === '3D' ? rightAdjustedDepthForShelves/2 - 1.5 : rightDepth/2 + 1.0]}>
+                        <mesh position={[rightWidth/2, sectionCenterY + sectionHeight/2 - basicThickness - 1.0, viewMode === '3D' ? -rightAdjustedDepthForShelves/2 + 0.5 : rightDepth/2 + 1.0]}>
                           <sphereGeometry args={[0.05, 8, 8]} />
                           <meshBasicMaterial color={dimensionColor} />
                         </mesh>
