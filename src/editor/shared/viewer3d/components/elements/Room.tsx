aea644731e1de002e57c26fe96856e506b18ce97
jinsoo <sbbc212@gmail.com>
2025-10-15 17:54:50 +0900

    fix: remove duplicate sub-frames from normal zone in right dropped ceiling case

--- src/editor/shared/viewer3d/components/elements/Room.tsx
+++ src/editor/shared/viewer3d/components/elements/Room.tsx
@@ -2735,27 +2735,6 @@ const Room: React.FC<RoomProps> = ({
 
           return (
             <>
-              {/* 우측 벽 안쪽 세로 서브프레임 (뒤쪽) */}
-              <group
-                position={[
-                  xOffset + width - frameThickness.right,
-                  droppedCenterY,
-                  furnitureZOffset + furnitureDepth/2 - mmToThreeUnits(END_PANEL_THICKNESS)/2 - mmToThreeUnits(30)
-                ]}
-                rotation={[0, Math.PI / 2, 0]}
-              >
-                <BoxWithEdges
-                  args={[
-                    mmToThreeUnits(40),
-                    droppedHeight,
-                    mmToThreeUnits(END_PANEL_THICKNESS)
-                  ]}
-                  position={[0, 0, 0]}
-                  material={rightSubFrameMaterial ?? new THREE.MeshStandardMaterial({ color: '#cccccc' })}
-                  renderMode={renderMode}
-                  shadowEnabled={shadowEnabled}
-                />
-              </group>
               {/* 우측 벽 안쪽 정면 프레임 (벽과 가구 사이 공간 메우기) */}
               <group
                 position={[
@@ -2772,27 +2751,6 @@ const Room: React.FC<RoomProps> = ({
                   ]}
                   position={[0, 0, 0]}
                   material={rightSubFrameMaterial ?? new THREE.MeshStandardMaterial({ color: '#cccccc' })}
-                  renderMode={renderMode}
-                  shadowEnabled={shadowEnabled}
-                />
-              </group>
-              {/* 좌측 벽 안쪽 세로 서브프레임 (뒤쪽) */}
-              <group
-                position={[
-                  xOffset + frameThickness.left,
-                  droppedCenterY,
-                  furnitureZOffset + furnitureDepth/2 - mmToThreeUnits(END_PANEL_THICKNESS)/2 - mmToThreeUnits(30)
-                ]}
-                rotation={[0, Math.PI / 2, 0]}
-              >
-                <BoxWithEdges
-                  args={[
-                    mmToThreeUnits(40),
-                    droppedHeight,
-                    mmToThreeUnits(END_PANEL_THICKNESS)
-                  ]}
-                  position={[0, 0, 0]}
-                  material={leftSubFrameMaterial ?? new THREE.MeshStandardMaterial({ color: '#cccccc' })}
                   renderMode={renderMode}
                   shadowEnabled={shadowEnabled}
                 />
