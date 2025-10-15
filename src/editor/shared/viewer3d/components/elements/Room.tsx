c0971acee494a5c1275b258932f3dd4e08f12ace
jinsoo <sbbc212@gmail.com>
2025-10-15 17:37:28 +0900

    fix: remove offset distances from frame positions (remove mmToThreeUnits(10), mmToThreeUnits(30), mmToThreeUnits(20))

--- src/editor/shared/viewer3d/components/elements/Room.tsx
+++ src/editor/shared/viewer3d/components/elements/Room.tsx
@@ -2647,9 +2647,9 @@ const Room: React.FC<RoomProps> = ({
               {/* 좌측 벽 안쪽 세로 서브프레임 (뒤쪽) */}
               <group
                 position={[
-                  xOffset + frameThickness.left - mmToThreeUnits(10),
+                  xOffset + frameThickness.left,
                   droppedCenterY,
-                  furnitureZOffset + furnitureDepth/2 - mmToThreeUnits(END_PANEL_THICKNESS)/2 - mmToThreeUnits(30)
+                  furnitureZOffset + furnitureDepth/2 - mmToThreeUnits(END_PANEL_THICKNESS)/2
                 ]}
                 rotation={[0, Math.PI / 2, 0]}
               >
@@ -2670,7 +2670,7 @@ const Room: React.FC<RoomProps> = ({
                 position={[
                   xOffset + frameThickness.left / 2,
                   droppedCenterY,
-                  furnitureZOffset + furnitureDepth/2 - mmToThreeUnits(20)
+                  furnitureZOffset + furnitureDepth/2
                 ]}
               >
                 <BoxWithEdges
@@ -2688,9 +2688,9 @@ const Room: React.FC<RoomProps> = ({
               {/* 우측 벽 안쪽 세로 서브프레임 (뒤쪽) */}
               <group
                 position={[
-                  xOffset + width - frameThickness.right + mmToThreeUnits(10),
+                  xOffset + width - frameThickness.right,
                   droppedCenterY,
-                  furnitureZOffset + furnitureDepth/2 - mmToThreeUnits(END_PANEL_THICKNESS)/2 - mmToThreeUnits(30)
+                  furnitureZOffset + furnitureDepth/2 - mmToThreeUnits(END_PANEL_THICKNESS)/2
                 ]}
                 rotation={[0, Math.PI / 2, 0]}
               >
@@ -2711,7 +2711,7 @@ const Room: React.FC<RoomProps> = ({
                 position={[
                   xOffset + width - frameThickness.right / 2,
                   droppedCenterY,
-                  furnitureZOffset + furnitureDepth/2 - mmToThreeUnits(20)
+                  furnitureZOffset + furnitureDepth/2
                 ]}
               >
                 <BoxWithEdges
@@ -2780,9 +2780,9 @@ const Room: React.FC<RoomProps> = ({
               {/* 우측 벽 안쪽 세로 서브프레임 (뒤쪽) */}
               <group
                 position={[
-                  xOffset + width - frameThickness.right + mmToThreeUnits(10),
+                  xOffset + width - frameThickness.right,
                   droppedCenterY,
-                  furnitureZOffset + furnitureDepth/2 - mmToThreeUnits(END_PANEL_THICKNESS)/2 - mmToThreeUnits(30)
+                  furnitureZOffset + furnitureDepth/2 - mmToThreeUnits(END_PANEL_THICKNESS)/2
                 ]}
                 rotation={[0, Math.PI / 2, 0]}
               >
@@ -2803,7 +2803,7 @@ const Room: React.FC<RoomProps> = ({
                 position={[
                   xOffset + width - frameThickness.right / 2,
                   droppedCenterY,
-                  furnitureZOffset + furnitureDepth/2 - mmToThreeUnits(20)
+                  furnitureZOffset + furnitureDepth/2
                 ]}
               >
                 <BoxWithEdges
@@ -2821,9 +2821,9 @@ const Room: React.FC<RoomProps> = ({
               {/* 좌측 벽 안쪽 세로 서브프레임 (뒤쪽) */}
               <group
                 position={[
-                  xOffset + frameThickness.left - mmToThreeUnits(10),
+                  xOffset + frameThickness.left,
                   droppedCenterY,
-                  furnitureZOffset + furnitureDepth/2 - mmToThreeUnits(END_PANEL_THICKNESS)/2 - mmToThreeUnits(30)
+                  furnitureZOffset + furnitureDepth/2 - mmToThreeUnits(END_PANEL_THICKNESS)/2
                 ]}
                 rotation={[0, Math.PI / 2, 0]}
               >
@@ -2844,7 +2844,7 @@ const Room: React.FC<RoomProps> = ({
                 position={[
                   xOffset + frameThickness.left / 2,
                   droppedCenterY,
-                  furnitureZOffset + furnitureDepth/2 - mmToThreeUnits(20)
+                  furnitureZOffset + furnitureDepth/2
                 ]}
               >
                 <BoxWithEdges
