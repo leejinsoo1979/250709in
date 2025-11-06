82d581fd0da5e26a808b1596abb8ebf42f17a8c8
jinsoo <sbbc212@gmail.com>
2025-11-06 18:07:14 +0900

    fix: replace remaining design file icon in file tree root level
    - Replace SVG pencil icon with AiOutlineFileMarkdown for root-level design files
    - Remove designIcon wrapper div
    - Now all design file icons in file tree use AiOutlineFileMarkdown consistently

--- src/pages/SimpleDashboard.tsx
+++ src/pages/SimpleDashboard.tsx
@@ -2922,14 +2922,7 @@ const SimpleDashboard: React.FC = () => {
                                 }}
                               >
                                 <div className={styles.treeItemIcon}>
-                                  <div className={styles.designIcon}>
-                                    <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2.5">
-                                      <path d="M12 19l7-7 3 3-7 7-3-3z"/>
-                                      <path d="M18 13l-1.5-7.5L2 2l3.5 14.5L13 18l5-5z"/>
-                                      <path d="M2 2l7.586 7.586"/>
-                                      <circle cx="11" cy="11" r="2"/>
-                                    </svg>
-                                  </div>
+                                  <AiOutlineFileMarkdown size={14} />
                                 </div>
                                 <span>{designFile.name}</span>
                                 <div className={styles.treeItemActions}>
