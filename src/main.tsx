25145c57a5f7640f6690553151ccae2738d1d91a
jinsoo <sbbc212@gmail.com>
2026-03-12 04:59:48 +0900

    fix: variables.css import 추가로 사이드바-뷰어 갭 해결
    variables.css가 main.tsx에서 import되지 않아 모든 CSS 변수
    (--sidebar-icon-width, --sidebar-content-width, --sidebar-total-width 등)가
    로드되지 않고 fallback 값만 사용됨. fallback 값 불일치로 갭 발생.
    variables.css를 theme.css 앞에 import하여 해결.

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import '@/styles/variables.css'
 import '@/styles/theme.css'
 import './index.css'
 import '@/styles/global.css'
